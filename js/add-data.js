f14f8bc6cacf9ef2b83cb6ef90f4fa0c7b1dd4ca
Alibahoos <gfa8825@gmail.com>
2025-04-23 21:02:24 +0330

    حذف تمام محتوای قبلی

--- js/add-data.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('dataForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('اطلاعات با موفقیت ذخیره شد!');
-    // در اینجا کد ذخیره به دیتابیس اضافه شود
-});
--- js/search.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('searchBtn').addEventListener('click', function() {
-    const query = document.getElementById('searchInput').value;
-    document.getElementById('results').innerHTML = 
-        `<p>نتایج جستجو برای: ${query}</p><p>این بخش در حال توسعه است...</p>`;
-});
